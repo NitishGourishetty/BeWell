41a801922e48965d52714840cb94e14cf8728939
Kevin Le <121095365+MrGrinchFx@users.noreply.github.com>
2024-03-17 19:12:38 -0700

    finished Calendar

--- Pages/SignUpPage.tsx
+++ Pages/SignUpPage.tsx
@@ -1,56 +1,53 @@
 import React from "react";
-import { View, Pressable } from "react-native";
-import { Text, TextField } from 'react-native-ui-lib'
-import { Dimensions } from "react-native" 
-import proximanova from "../assets/fonts/proxima-nova/proximanova-regular.otf"
+import { ScrollView, View, Pressable } from "react-native";
+import { Image, Text, TextField } from 'react-native-ui-lib';
+import { Dimensions } from "react-native";
+import { useFonts } from "expo-font";
+import { MARGIN_KEY_PATTERN } from "react-native-ui-lib/src/commons/modifiers";
 
 export default function SignUpPage(){
     const width = Dimensions.get("window").width * 0.9;
     const height = Dimensions.get("window").height * 0.9;
+    const [fontsLoaded, fontError] = useFonts({
+        'proxima-nova-regular': require('../assets/fonts/proxima-nova/proxima-nova-regular.otf'),
+        'proxima-nova-bold' : require('../assets/fonts/proxima-nova/proxima-nova-bold.otf'),
+        'proxima-nova-semibold' : require('../assets/fonts/proxima-nova/proxima-nova-semibold.otf')
+    });
+
     return(
-        <View style={{flex : 1, alignItems : "center", justifyContent : "center", position : "absolute"}}>
-            <Text style={{fontSize : height/10, position : "absolute", bottom : height/3, textAlign : "center", color : "#498C68", width : width}}>
-                Sign Up
+        <ScrollView contentContainerStyle={{flex : 1, alignItems : "center", justifyContent : "center", backgroundColor : "E6D6B8"}}>
+            <Text style={{fontSize : height/20, textAlign : "center", color : "#498C68", fontFamily : 'proxima-nova-semibold', marginBottom : 50}}>
+                {'What should \n we call you?'}
             </Text>
-            <View style={{}}>
+            {/* <Image 
+                source={require('../assets/img/sign-up.png')}
+                style={{resizeMode : "center"}}
+            /> */}
+            <View style={{width: width * 0.8}}>
                 <TextField
-                color="#498C68"
-                containerStyle={{width : width / 2, borderBottomColor: '#AFC689', borderBottomWidth: 1, height : height / 17}}
-                fieldStyle={{}}
-                placeholder={'username'}
-                floatingPlaceholderColor="#AFC689"
-                floatingPlaceholder
-                // onChangeText={}
-                enableErrors
-                validate={['required', (value) => value.length > 6]}
-                // useful code: validate={['required', 'email', (value) => value.length > 6]}
-                validationMessage={['Field is required', 'Password is too short']}
-                // showCharCounter
-                // maxLength={30}
-                underlineColorAndroid="#AFC689"
+                    color="#498C68"
+                    containerStyle={{borderBottomColor: '#AFC689', borderBottomWidth: 1}}
+                    placeholder={'username'}
+                    selectionColor="#AFC689"
+                    floatingPlaceholderColor="#AFC689"
+                    floatingPlaceholder
+                    enableErrors
+                    validate={['required', (value) => value.length > 6]}
+                    validationMessage={['Field is required', 'Password is too short']}
+                    underlineColorAndroid="#AFC689"
                 />
-
-            </View>
-            
-
-
-            <View style={{}}>
                 <TextField
-                color="#498C68"
-                containerStyle={{width : width / 2, borderBottomColor: '#AFC689', borderBottomWidth: 1, height : height / 17}}
-                fieldStyle={{}}
-                placeholder={'password'}
-                floatingPlaceholderColor="#AFC689"
-                floatingPlaceholder
-                // onChangeText={}
-                enableErrors
-                validate={['required', (value) => value.length > 6]}
-                // useful code: validate={['required', 'email', (value) => value.length > 6]}
-                validationMessage={['Field is required', 'Password is too short']}
-                // showCharCounter
-                // maxLength={30}
-            />
+                    color="#498C68"
+                    containerStyle={{borderBottomColor: '#AFC689', borderBottomWidth: 1}}
+                    placeholder={'password'}
+                    floatingPlaceholderColor="#AFC689"
+                    floatingPlaceholder
+                    enableErrors
+                    validate={['required', (value) => value.length > 6]}
+                    validationMessage={['Field is required', 'Password is too short']}
+                    underlineColorAndroid="#AFC689"
+                />
             </View>
-        </View>
+        </ScrollView>
     )
 }
