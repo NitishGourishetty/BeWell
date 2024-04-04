011ef79eb31bf0c2b125137a55d9d63833d1165d
Person2 <57010873+AbhinavTata@users.noreply.github.com>
2024-04-03 17:07:04 -0700

    killing myself

--- Pages/ProfilePicturePage.tsx
+++ Pages/ProfilePicturePage.tsx
@@ -1,32 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import * as React from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { Image, View, Button, StyleSheet } from 'react-native';
-import { launchImageLibrary } from 'react-native-image-picker';
+import * as ImagePicker from 'react-native-image-picker';
 import { useCustomFonts } from "../assets/fonts/fontDeclarations";
-import { PERMISSIONS, request } from 'react-native-permissions'
 
 export default function ProfilePicturePage(){
     useCustomFonts();
     const [imageSource, setImageSource] = useState(null);
-    // useEffect(() => {
-    //     request(PERMISSIONS.IOS.MEDIA_LIBRARY); // Request permission on component mount
-    // }, []);
-    const selectImage = () => {
-        console.log("start");
-        launchImageLibrary({ mediaType : "photo"}, (response) => {
-            console.log("fake success");
-            if (!response.didCancel) {
-                console.log("real success");
-                setImageSource({ uri: response.assets[0].uri  });
-            }
+    const openImagePicker = () => {
+        ImagePicker.launchImageLibrary({ mediaType : "photo"}, (response) => {
+          if (response.didCancel) {
+            console.log('User cancelled image picker');
+          } else if (response.errorCode) {
+            console.log('Image picker error: ', response.errorMessage);
+          } else {
+            let imageUri = response.assets?.[0]?.uri;
+            setImageSource(imageUri);
+          }
         });
-        console.log("end");
-    }
+      };
         return (
             <View style={styles.container}>
                 {imageSource && (
                     <Image source={{ uri: imageSource }} style={styles.image} resizeMode="cover" />
                 )}
-                <Button title="Select Image" onPress={selectImage} color="#AFC689"/>
+                <Button title="Select Image" onPress={openImagePicker} color="#AFC689"/>
             </View>
         )
 };
