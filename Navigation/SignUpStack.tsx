4e9ca428861c6b60a4ed662f7ecab22568d7cf9d
MrGrinchFx <2003kevinle@gmail.com>
2024-04-09 00:34:13 -0700

    merging

--- Navigation/SignUpStack.tsx
+++ Navigation/SignUpStack.tsx
@@ -8,6 +8,7 @@ import ProfilePicturePage from '../Pages/ProfilePicturePage';
 import NamePage from '../Pages/NamePage';
 import SignUpPage from '../Pages/SignUpPage';
 import Account from '../Pages/TempAccountPage';
+import
 
 const signUpFlow = createNativeStackNavigator();
 
@@ -15,9 +16,10 @@ export default function SignUpStack() {
     return (
         <signUpFlow.Navigator screenOptions={{ animationTypeForReplace: "pop" }}>
             <signUpFlow.Screen name={"Login"} component={LoginPage} />
-            <signUpFlow.Screen name={"Sign Up"} component={Account} />
+            <signUpFlow.Screen name={"Sign Up"} component={SignUpPage} />
             <signUpFlow.Screen name={"Name"} component={NamePage} />
             <signUpFlow.Screen name={"Profile Picture"} component={ProfilePicturePage} />
+
         </signUpFlow.Navigator>
     )
 }
