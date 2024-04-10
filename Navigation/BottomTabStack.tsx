2b67abd5f63df50482b06aaf78cd96e1c36116ea
MrGrinchFx <2003kevinle@gmail.com>
2024-04-09 19:58:07 -0700

    idk

--- Navigation/BottomTabStack.tsx
+++ Navigation/BottomTabStack.tsx
@@ -14,6 +14,7 @@ import UserPwdPage from '../Pages/UserPwdPage';
 import ProfilePicturePage from '../Pages/ProfilePicturePage';
 import TempSignUpPage from '../Pages/TempSignUp';
 import { Session } from '@supabase/supabase-js'
+import Feed from '../Pages/BeWellFeed/Feed';
 
 
 const BottomTabStack = createNativeStackNavigator()
@@ -65,7 +66,7 @@ export function MainStack() {
         >
             <BottomTabStack.Screen name="Home" component={MainHomePage} />
             <BottomTabStack.Screen name="Login" component={LoginPage} />
-            <BottomTabStack.Screen name="Friends" component={FriendsListPage} />
+            <BottomTabStack.Screen name="Feed" component={Feed} />
             <BottomTabStack.Screen name="Testing" component={UserPwdPage} />
         </Tab.Navigator>
     )
