e56d172b6d1bee4f0a924aef3c62b69441886088
MrGrinchFx <2003kevinle@gmail.com>
2024-04-16 12:13:13 -0700

    merge

--- Navigation/SignUpStack.tsx
+++ Navigation/SignUpStack.tsx
@@ -12,6 +12,7 @@ import SetGoalsPage from '../Pages/SignUpFlowPages/SetGoalsPage';
 import GoalSetupPage from '../Pages/SignUpFlowPages/GoalSetupPage';
 import TimePage from '../Pages/SignUpFlowPages/TimePage';
 import PrivacySetupPage from '../Pages/SignUpFlowPages/PrivacySetupPage';
+import { MainStack } from '../Navigation/BottomTabStack';
 
 
 const signUpFlow = createNativeStackNavigator();
@@ -27,6 +28,7 @@ export default function SignUpStack() {
             <signUpFlow.Screen name={"GoalSetup"} component={GoalSetupPage} />
             <signUpFlow.Screen name={"TimePage"} component={TimePage} />
             <signUpFlow.Screen name={"PrivacySetup"} component={PrivacySetupPage} />
+            <signUpFlow.Screen name={"MainStack"} component={MainStack} />
         </signUpFlow.Navigator>
     )
 }
--- Pages/BeWellFeed/Feed.tsx
+++ Pages/BeWellFeed/Feed.tsx
@@ -2,17 +2,26 @@ import * as React from 'react'
 import { useState, useEffect } from 'react'
 import { View, Text } from 'react-native'
 import { getUsersHabits } from '../../lib/backend'
+import { supabase } from '../../lib/supabase'
+import { Session } from '@supabase/supabase-js'
 
-export default function Feed({ session }) {
+export default function Feed() {
 
+    const [session, setSession] = useState<Session | null>(null)
     async function getHabits() {
         const data = await getUsersHabits(session);
         console.log(data);
     }
 
     useEffect(() => {
-        getHabits();
-    }, []);
+        supabase.auth.getSession().then(({ data: { session } }) => {
+            setSession(session)
+        })
+        supabase.auth.onAuthStateChange((_event, session) => {
+            setSession(session)
+        })
+    }, [])
+
 
     return (
         <View>
