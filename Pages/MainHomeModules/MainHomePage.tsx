d4aff13efec1f24de30f358c30df1c3b51564cdf
Nitish Gourishetty <nitishgourishetty@gmail.com>
2024-04-17 23:09:46 -0700

    yum

--- Pages/MainHomeModules/MainHomePage.tsx
+++ Pages/MainHomeModules/MainHomePage.tsx
@@ -6,9 +6,50 @@ import { useCustomFonts } from "../../assets/fonts/fontDeclarations";
 import StreaksModule from "./StreaksModule";
 import CalendarModule from "./CalendarModule";
 import HabitsModule from "./HabitsModule";
+import { useEffect, useState } from "react";
+import { supabase } from "../../lib/supabase";
+import { Session } from "@supabase/supabase-js";
+import { addHabit, getUsersHabits } from "../../lib/backend";
 
 
 export default function MainHomePage() {
+    const [session, setSession] = useState<Session | null>(null)
+    const [loading, setLoading] = useState(true)
+    const [habitData, setHabitData] = useState(null)
+
+    useEffect(() => {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+          setSession(session)
+    
+        })
+        supabase.auth.onAuthStateChange((_event, session) => {
+          setSession(session)
+        })
+
+      }, [])
+
+      async function getHabitInfo() {
+        try {
+            setLoading(true)
+          if (!session?.user) throw new Error('No user on the session!')
+          let data = await getUsersHabits(session);
+          if(data) {
+           //Do Stuff with data
+           alert(data[0].habit_info)
+           setHabitData(data)
+          }
+        } catch (error) {
+          if (error instanceof Error) {
+            alert(error.message)
+            alert("ERROR")
+          }
+        } finally {
+            setLoading(false)
+        }
+      }
+      useEffect(() => {
+        if (session) getHabitInfo()
+      }, [session])
     const user = "user";
     const { width, height } = useWindowDimensions()
     useCustomFonts();
@@ -29,9 +70,10 @@ export default function MainHomePage() {
                     <Text style={styles.Subheading}>
                         Habits
                     </Text>
-                    <HabitsModule habitName={"Defeating The Harkonnens"} time={10} index={0} />
-                    <HabitsModule habitName={"Talk to Jannii"} time={7} index={1} />
-                    <HabitsModule habitName={"Fight the Holy War"} time={12} index={2} />
+
+                    <HabitsModule habitName={habitData[0].habit_info} time={10} index={0} />
+                    <HabitsModule habitName={habitData[1].habit_info} time={7} index={1} />
+                    <HabitsModule habitName={habitData[2].habit_info} time={12} index={2} />
 
                 </View>
             </ScrollView>
